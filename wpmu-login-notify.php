28526aee77190e9a2ff8e18a56201c6c18ba63a5
Jason Jersey <icryptic@users.noreply.github.com>
2015-06-08 19:42:11 -0600

    Update wpmu-login-notify.php

--- wpmu-login-notify.php
+++ wpmu-login-notify.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: WPMU Login Notification
- * Description: Send an email to the site admin when someone successfully logs into their sites wp-admin. Optional, Edit line 23 to specify a sender email.
+ * Description: Send an email to the site admin when someone successfully logs into their sites wp-admin. Optional, uncomment line 31 to use site email as sender email on non WPMU sites, otherwise the default WP email is used (ie: wordpress@mysite.com). On multisite, the Network Admin email is used as the sender email. You can change it via wp-admin/network/settings.php.
  * Author: Jason Jersey
  * Version: 1.0
  */
@@ -21,7 +21,15 @@ function send_email_on_login($username) {
     $subject = "Login Alert for $name";
     $user = get_user_by( 'login', $username );
     $to   = $user->user_email;
-    //$headers = "From: Admin <no-reply@mychildsvillage.com>";
+
+    if (function_exists('is_multisite') && is_multisite()) { 
+        $admin_name = get_site_option( 'name' );
+        $admin_email = get_site_option( 'admin_email' );
+        $headers = "From: $admin_name <$admin_email>";
+    } else {
+        $admin_email = get_option('admin_email');
+        //$headers = "From: Admin <$admin_email>"; 
+    }
     
     /* The unique token can be inserted in the message with %s */
     $message = "Your account was recently logged into from a device. Was this you?
